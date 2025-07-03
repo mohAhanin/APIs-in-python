056d1a46f7010b56e3ff58473cc94f2f2bdd780c
unknown <maahaninir@gmail.com>
2025-07-03 14:59:23 +0330

    session 2 updated

---
+++ Course/session02/session02.py
@@ -0,0 +1,112 @@
+# Review
+# name = input("what is your name")
+# print(name)
+# age = input("how old are you?")
+# print(type(age))
+# a = 'c'
+# String = "mohammadali"
+# number = 1234
+# flnumber = 14.58
+# state = False
+
+# a = 8
+# b = 3
+
+# # addition
+# print ('Sum: ', a + b)  # 11
+# # subtraction
+# print ('Subtraction: ', a - b)  # 5
+# # multiplication
+# print ('Multiplication: ', a * b)  # 24 
+# # division
+# print ('Division: ', a / b)  # 2.6666666666666665
+# # floor division
+# print ('Floor Division: ', a // b)  # 2
+# # modulo
+# print ('Modulo: ', a % b)   # 2
+# # a to the power b
+# print ('Power: ', a ** b)  # 512
+
+
+
+# a = 12
+# a = a + 1
+# a += 1
+# print(a)
+# a = a - 1
+# a -= 1
+# print(a)
+
+# a = 3
+# b = 3
+# c = a == b
+# print(c)
+
+# print(3==4)
+# print(3<=4)
+
+# And
+# A   # B   # And
+# 0    0     0
+# 0    1     0
+# 1    0     0
+# 1    1     1
+
+# OR
+# A   # B   # OR
+# 0    0     1
+# 0    1     1
+# 1    0     1
+# 1    1     0
+
+
+# a = 1
+# b = 0
+
+#print((a and b) or (a and a))
+
+# print(not(a))
+
+# X = 12
+# y = 13
+# print(X is not y)
+
+# name = "mohammad"
+# name2 = "mohammad ali"
+# print(name in name2)
+# print(name2 in name)
+
+# a = 123
+# b = 1234
+# a = str(a)
+# b = str(b)
+# print(a in b)
+
+# a = 12
+# print(a)
+# b = 13 
+# print(a & b)
+
+
+# binary
+# 1 = 00000001 1*(2**0)
+# 2 = 00000010 1*(2**1)
+# 3 = 00000011 1*(2**0)+ 1*(2**1)
+# 4 = 00000100 0*(2**0) + 0*(2**1) + 1*(2**2) 
+# ------------------------
+#  7 6 5 4 3 2 1 0 
+#  0 0 0 0 0 0 0 1
+
+# a = 11
+# b = 12
+
+# c = a == b # False
+# d = a != b # True
+# print(c)
+# print(d)
+
+# e = c & d 
+# print(e)
+
+print(9 << 2)  # Output: 36
+# 9 = 0000000000001001  -> 36 = 0000000000100100
